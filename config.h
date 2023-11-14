3ab992c343d8d347cf6d0f5c2476d90ba38dab7b
evilwaveforms <8681846+evilwaveforms@users.noreply.github.com>
2023-11-14 22:46:00 +0200

    Change font

--- config.h
+++ config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DinaRemasterII:pixelsize=32:antialias=true:autohint=true";
+/* static char *font = "DinaRemasterII:pixelsize=32:antialias=true:autohint=true"; */
+static char *font = "Overpass Mono:pixelsize=28:antialias=false:autohint=true";
 static int borderpx = 2;
 
 /*
